80ce8677d04ba7e58de014d58bc139274b14e38d
Murugaraj Anandan <murugarajanandan.t@gmail.com>
2020-02-27 07:10:13 -0800

    inital commit

---
+++ API/WebAPI/Models/ProjectDBModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class ProjectDBModel
+    {
+        public int? ProjectID { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Priority { get; set; }
+
+        public string ProjectTitle { get; set; }
+
+        public int? UserID { get; set; }
+
+        
+    }
+}
